3901020bd949dbd8cbec820c1cdd75e21eab788a
Sowmya-Vejandla <sowmyavejandla2001@gmail.com>
2024-04-16 10:15:24 +0530

    Simon Game Project

---
+++ README.md
@@ -0,0 +1,8 @@
+## Project Title: Simon Game
+
+
+## Description:
+### The Simon Game is a classic memory game developed solely using HTML, CSS, and JavaScript. As the sole developer, I designed and implemented the game from scratch. Players are presented with a sequence of coloured buttons, each accompanied by a unique sound. The objective is to replicate the sequence correctly by clicking on the buttons in the same order. If a wrong button is clicked, the game ends, and the player's score is displayed.
+
+## Outcome:
+### Through this project, I gained valuable experience in web development and honed my skills in HTML, CSS, and JavaScript. I deepened my understanding of DOM manipulation, event handling, and game logic implementation. Additionally, I learned about the importance of user experience design and feedback mechanisms in creating engaging and intuitive web applications. This project allowed me to showcase my ability to work independently and deliver a functional and enjoyable game experience to users.
---
+++ app.js
@@ -0,0 +1,94 @@
+let gameSeq=[];
+let userSeq=[];
+
+let started=false;
+let level=0;
+
+let btns=["red","yellow","green","blue"];
+
+let h2=document.querySelector("h2");
+
+document.addEventListener("keypress", function(){
+    if(started==false){
+        console.log("game started");
+        started=true;
+
+        levelUp();
+    }
+
+    
+});
+
+function gameFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash");
+    },250);
+}
+
+function userFlash(btn){
+    btn.classList.add("userflash");
+    setTimeout(function(){
+        btn.classList.remove("userflash");
+    },250);
+}
+
+function levelUp(){
+    userSeq=[];
+    level++;
+    h2.innerText=`Level ${level}`;
+
+    let randInd=Math.floor(Math.random()*3);
+    let randColor=btns[randInd];
+    let randBtn=document.querySelector(`.${randColor}`);
+    // console.log(randInd);
+    // console.log(randColor);
+    // console.log(randBtn);
+    
+    gameSeq.push(randColor);
+    console.log(gameSeq);
+    gameFlash(randBtn);
+}
+
+function checkAns(idx){
+    // console.log("current level: ", level);
+    
+
+    if(userSeq[idx]==gameSeq[idx]){
+        if(userSeq.length==gameSeq.length){
+            setTimeout(levelUp,1000);
+        }
+    }else{
+        h2.innerHTML=`Game Over! Your Score was <b>${level}</b> <br> Press any key to start.`;
+        document.querySelector("body").style.backgroundColor="red";
+        setTimeout(function(){
+            document.querySelector("body").style.backgroundColor="white";
+        },150);
+        reset();
+    }
+}
+
+function btnPress(){
+    // console.log(this);
+    let btn=this;
+    userFlash(btn);
+
+    userColor=btn.getAttribute("id");
+    userSeq.push(userColor);
+
+    checkAns(userSeq.length-1);
+}
+
+let allBtns=document.querySelectorAll(".btn");
+for (btn of allBtns){
+    btn.addEventListener("click",btnPress);
+}
+
+function reset(){
+    started=false;
+    gameSeq=[];
+    userSeq=[];
+    level=0;
+}
+
+
